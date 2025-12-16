a9fdda084bd295bbb35698254ad03f6c0f65ae98
Akira Jeon <ndivinity69@gmail.com>
2025-12-15 20:36:05 -0600

    Started package and added checking for integer 0

---
+++ index.js
@@ -0,0 +1,20 @@
+const METADATA = {
+    Version: "0.0.1"
+};
+
+function isInteger(int) {
+    if(int == undefined || int == null) {
+        return true;
+    } else if(int == 0 || int == '0') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+module.exports = {
+    METADATA,
+    isInteger
+};
